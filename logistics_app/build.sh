59d1a8682d034401f3f2af53b23d6c64c9dffd8b
Sheetal Joshi <sheetjos@amazon.com>
2023-01-26 17:17:15 -0500

    Fixed tags

--- logistics_app/build.sh
+++ logistics_app/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_REPO=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$APP_IMAGE_NAME:$IMAGE_TAG
+IMAGE_REPO=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$APP_IMAGE_NAME:$APP_IMAGE_TAG
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin $IMAGE_REPO
 docker buildx use builder
 docker buildx build --push --platform linux/arm64,linux/amd64 -t $IMAGE_REPO .
