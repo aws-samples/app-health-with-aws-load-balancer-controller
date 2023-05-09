4d2361c05f0ca8de0773ebdee791a7261a7d925e
Biran <birayaha@3c063041fedb.ant.amazon.com>
2023-05-09 09:07:34 -0700

    githubaction

--- simple-multiarch-app/ci-pipeline.sh
+++ simple-multiarch-app/ci-pipeline.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-
+#!/bin/bash -x
+npm install cdk-nag
+npm install -g aws-cdk@latest
 npm install aws-cdk-lib
-. ~/.bash_profile
 cdk bootstrap aws://$AWS_ACCOUNT_ID/$AWS_REGION
 npm install
 cdk deploy \
---app "npx ts-node --prefer-ts-exts ./ci-pipeline.ts" --parameters BUILDXVER=$BUILDX_VER --parameters APPIMAGENAME=$APP_IMAGE_NAME --parameters APPIMAGETAG=$APP_IMAGE_TAG --parameters APPIMAGEARMTAG=$APP_IMAGE_ARM_TAG --parameters APPIMAGEAMDTAG=$APP_IMAGE_AMD_TAG
+--app "npx ts-node --prefer-ts-exts ./ci-pipeline.ts" --parameters BUILDXVER=$BUILDX_VER --parameters APPIMAGENAME=$APP_IMAGE_NAME --parameters APPIMAGETAG=$APP_IMAGE_TAG --parameters APPIMAGEARMTAG=$APP_IMAGE_ARM_TAG --parameters APPIMAGEAMDTAG=$APP_IMAGE_AMD_TAG --parameters GITHUBOAUTHTOKEN=$GITHUB_OAUTH_TOKEN --parameters GITHUBREPO=$GITHUB_REPO --parameters GITHUBUSER=$GITHUB_USER --parameters GITHUBBRANCH=$GITHUB_BRANCH
