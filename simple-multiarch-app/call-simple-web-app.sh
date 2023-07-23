88357d2e9ae0df5b0589e59b3e4af613dc648038
yahavb <yahavb@amazon.com>
2023-07-23 07:09:40 -0700

    add response time metric

--- simple-multiarch-app/call-simple-web-app.sh
+++ simple-multiarch-app/call-simple-web-app.sh
@@ -1,12 +1,8 @@
 #!/bin/bash -x
-TIMEFORMAT=' %R'
 while true
 do
-  app_ret=`time curl -o /dev/null -w '%{http_code}' -sL ${APP_URL}/runtime/` 
-  http_code=`echo $app_ret| awk '{print $1}'`
-  http_response_time=`echo $app_ret| awk '{print $2}'`
-  if [ "$http_code" == 200 ]; then
-    aws cloudwatch put-metric-data --metric-name http_response_time --namespace ${DEPLOY_NAME} --value $http_response_time
-  fi
+  cmd="curl -o /dev/null -sL ${APP_URL}/runtime/" 
+  http_response_time=`TIMEFORMAT="%R"; { time $cmd; } 2>&1`
+  aws cloudwatch put-metric-data --metric-name http_response_time --namespace ${DEPLOY_NAME} --value $http_response_time
   sleep $SLEEP_BETWEEN_CYCLE
 done
